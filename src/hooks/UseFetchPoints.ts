3f222b1a9c46802d5c05c07a1366aa692c7b326b
Irene <irespinoza.dwfs13@bootcampudd.cl>
2025-01-08 02:04:03 -0300

    feat: añadir hook personalizado para obtener puntos desde el backend

---
+++ src/hooks/UseFetchPoints.ts
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export interface Pointdata {
+    id: string;
+    latitud: number;
+    longitude:number;
+    name: string;
+    description: string;
+    photo_url: string;
+    address: string;
+    commune: string;
+    region: string;
+    gallery?: { 
+        galleryName: string;
+        localNumber: string;
+    };
+}
+
+const UseFetchPoints = () => {
+    const [points, setPoints] = useState<Pointdata[]>([]);
+    const [loading, setLoading] =useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect (()=> {
+        const fetchPoints = async () => {
+            try {
+                const response= await axios.get('http://localhost:3000/points');
+                setPoints(response.data);
+            } catch (error) {
+                console.error('Error al obtener los marcadores:', error);
+                setError('Error al obtener los maracadores');
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchPoints();
+    }, []);
+
+    return {points, loading, error};
+};
+
+export default UseFetchPoints; 
