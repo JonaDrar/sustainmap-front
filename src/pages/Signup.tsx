5742dfa02943ccbedf8f1cf9657461feaff83b66
Irene <irespinoza.dwfs13@bootcampudd.cl>
2025-01-19 00:03:48 -0300

    fix:Resolución de conflictos al aplicar stash

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -55,19 +55,19 @@ const Signup: React.FC = () => {
   };
   const isValidInput = Boolean(email.length && password.length && confirmPassword.length);
   return (
-    <div className="gradient-background flex items-center justify-center" style={{ minHeight: 'calc(100vh - 60px)' }}>
-      <div className="bg-white shadow-md rounded-xxl p-8 w-full max-w-md login-card">
+    <div className="gradient-background flex items-center justify-center p-4" style={{ minHeight: 'calc(100vh - 60px)' }}>
+      <div className="bg-white shadow-md rounded-xxl px-6 py-8 w-full max-w-xs sm:max-w-sm md:max-w-md login-card">
         <div className="mb-6 text-center">
           <img
             src="/images/logo-mot.png"
             alt="Matter of Trust Logo"
-            className="mx-auto login-logo"
+            className="w-24 sm:w-28 lg:w-36 mx-auto mb-4 login-logo"
           />
-          <h2 className="login-header">Iniciar sesión</h2>
-          <p className="text-sm text-gray-700 login-sub-header">Bienvenido/a<br /> Ingresa tus datos para comenzar.</p>
+          <h2 className=" text-lg md:text-2xl font-bold text-blue-700 login-header">Crear Cuenta </h2>
+          <p className="text-sm text-gray-700 login-sub-header-2">Bienvenido/a<br /> Ingresa tus datos para comenzar.</p>
         </div>
 
-        <form className="login-form" onSubmit={handleSignup}>
+        <form className=" space-y-4 login-form" onSubmit={handleSignup}>
 
           <div>
             <input
@@ -75,8 +75,9 @@ const Signup: React.FC = () => {
               placeholder="Ingresa tu correo"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-2 text-sm border rounded-md"
             />
-            {errors.email && <p className="error-text">{errors.email}</p>}
+            {errors.email && <p className="error-text text-sm">{errors.email}</p>}
           </div>
           <div>
             <input
@@ -87,8 +88,9 @@ const Signup: React.FC = () => {
                 setPassword(e.target.value);
                 validatePassword(e.target.value);
               }}
+              className="w-full px-4 py-2 text-sm border rounded-md"
             />
-            {errors.password && <p className="error-text">{errors.password}</p>}
+            {errors.password && <p className="error-text text-sm">{errors.password}</p>}
             {showValidationRequirements ? (<div className="password-tooltip">
               <ul>
                 <li style={{ color: passwordChecks.length ? 'green' : 'red' }}>
@@ -111,8 +113,9 @@ const Signup: React.FC = () => {
               placeholder="Repite tu contraseña"
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full px-4 py-2 text-sm border rounded-md"
             />
-            {errors.confirmPassword && <p className="error-text">{errors.confirmPassword}</p>}
+            {errors.confirmPassword && <p className="error-text text-sm">{errors.confirmPassword}</p>}
           </div>
 
           <button
@@ -126,7 +129,7 @@ const Signup: React.FC = () => {
             Registrarse
           </button>
         </form>
-        <p className="mt-4 text-center text-md text-gray-400">
+        <p className="mt-6 text-center text-sm text-gray-400">
           ¿Ya tienes una cuenta?
           {' '}
           <Link to="/" 
