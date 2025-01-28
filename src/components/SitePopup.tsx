5edc3028f6e776e381e19ac862a421b4b00b78fa
Irene <irespinoza.dwfs13@bootcampudd.cl>
2025-01-28 18:49:09 -0300

    fix: arreglar codigo y ajustar para modal de confirmación de elminar un punto

--- src/components/SitePopup.tsx
+++ src/components/SitePopup.tsx
@@ -1,6 +1,5 @@
 import { Popup } from "react-leaflet";
 import { Pointdata } from "../hooks/UseFetchPoints";
-import { MapPinIcon } from "@heroicons/react/24/solid";
 import DropdownButton from "./DropdownButton";
 import { useNavigate } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
@@ -22,6 +21,7 @@ const SitePopup: React.FC<SitePopupProps> = ({ site, onDeletePoint }) => {
     onDeletePoint(site.id);
   };
 
+
   const { loggedInUser } = useContext(UserContext);
   return (
     <Popup>
@@ -37,26 +37,35 @@ const SitePopup: React.FC<SitePopupProps> = ({ site, onDeletePoint }) => {
         {loggedInUser ? (
           // Botón desplegable
           <div className="ml-2">
-            <DropdownButton onEdit={handleEdit} onDelete={handleDelete} />
+            <DropdownButton
+              onEdit={handleEdit}
+              onDelete={handleDelete}
+              pointName={<span className="text-blue-500 font-bold">{site.name}</span>}
+            />
           </div>
         ) : null}
 
         {/* Contenido del cuerpo */}
         <div className="p-4">
-          {/* Nombre del salón y galería (opcional) */}
-          <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-2">
-            <h3 className="text-blue-700 font-bold text-base sm:text-lg">{site.name}</h3>
+          {/* Nombre del salón, galería y local */}
+          <div className="flex flex-col md:flex-row md:items-center md:justify-between mb-2">
+            {/* Contenedor izquierdo: Nombre */}
+            <h3 className="text-blue-700 font-bold text-base md:text-lg">
+              {site.name}
+            </h3>
+
+            {/* Contenedor derecho: Galería y Local */}
             {site.gallery && (
-              <div className="flex flex-col sm:flex-row sm:gap-2 text-green-600 font-thin text-xs sm:text-sm">
+              <div className="flex flex-col md:flex-row md:items-center md:gap-3 text-green-600 font-thin text-xs md:text-sm mt-1 md:mt-0">
                 {site.gallery.galleryName && (
-                  <span>
+                  <div className="whitespace-nowrap">
                     <strong>Galería:</strong> {site.gallery.galleryName}
-                  </span>
+                  </div>
                 )}
                 {site.gallery.localNumber && (
-                  <span>
+                  <div className="whitespace-nowrap">
                     <strong>Local:</strong> {site.gallery.localNumber}
-                  </span>
+                  </div>
                 )}
               </div>
             )}
@@ -64,7 +73,11 @@ const SitePopup: React.FC<SitePopupProps> = ({ site, onDeletePoint }) => {
 
           {/* Dirección */}
           <div className="flex items-center text-gray-700 text-xs sm:text-sm font-sans mb-2">
-            <MapPinIcon className="h-5 w-5 sm:h-6 sm:w-6 text-black mr-1" />
+           <img
+             src="/images/location-popup.png"
+             alt="Location"
+             className="h-4 w-4 sm:h-5 sm:w-5 md:h-6 md:w-6 mr-2 sm:mr-3" // Tamaños ajustables
+           />
             <p>
               {site.address}, {site.commune}. {site.region}.
             </p>
