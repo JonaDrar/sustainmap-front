490f603028e45bc485e950ab549ced71e2fa87ae
Irene <irespinoza.dwfs13@bootcampudd.cl>
2025-01-28 18:57:46 -0300

    feat:agregar codigo para modal de confirmación de eliminar punto

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -8,6 +8,7 @@ import SidebarMenu from "../components/SidebarMenu";
 import L from "leaflet";
 import { Pointdata } from "../hooks/UseFetchPoints";
 import SearchBar from "../components/SearchBar";  // Importa el SearchBar
+import SuccessModal from "../components/SucessModal";
 
 const LocateUser = ({
   onLocationFound,
@@ -110,6 +111,7 @@ const Map = () => {
   } | null>(null);
   const [filteredPoints, setFilteredPoints] = useState<Pointdata[]>([]);
   const { points, deletePoint } = UseFetchPoints();
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
 
   const pointData: Pointdata[] = points.map((point) => ({
     id: point.id,
@@ -141,6 +143,15 @@ const Map = () => {
     );
     setFilteredPoints(filtered);
   };
+  
+  const handleDeletePoint = (id: string, name: string) => {
+    deletePoint(id);
+    setSuccessMessage(name); // Solo guardamos el nombre de la peluquería
+  };
+
+  const closeSuccessModal = () => {
+    setSuccessMessage(null);
+  };
 
   useEffect(() => {
     const savedUserCoords = localStorage.getItem("userCoords");
@@ -168,45 +179,51 @@ const Map = () => {
 
   return (
     <div className="flex flex-col md:flex-row h-screen bg-white">
-    {/* Barra Lateral */}
-    <div className="order-2 md:order-1 md:w-2/5 lg:w-2/5 w-full h-1/3 md:h-full bg-white overflow-y-auto p-4">
-      <SidebarMenu
-        points={filteredPoints}
-        onPointSelect={(coords) => setSelectedCoords(coords)}
-        userCoords={userCoords}
+      {/* Barra Lateral */}
+      <div className="order-2 md:order-1 md:w-2/5 lg:w-2/5 w-full h-1/3 md:h-full bg-white overflow-y-auto p-4 relative">
+        <SidebarMenu
+          points={filteredPoints}
+          onPointSelect={(coords) => setSelectedCoords(coords)}
+          userCoords={userCoords}
+          onDeletePoint={(id, name) => handleDeletePoint(id,name)}
+        />
+      </div>
+
+      {/* Mapa */}
+      <div className="order-1 md:order-2 flex-grow w-full h-2/3 md:h-full relative">
+        <MapContainer
+          center={[-33.4489, -70.6693]}
+          zoom={9}
+          className="h-full w-full z-0"
+        >
+          <TileLayer
+            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/'>CARTO</a>"
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+          />
+          <MarkerList sites={pointData} onDeletePoint={(id, name) => handleDeletePoint(id, name)} />
+          <CenterMap coords={selectedCoords} />
+          <MapBoundsUpdater
+            points={pointData}
+            setFilteredPoints={setFilteredPoints}
+            resetSelectedCoords={() => setSelectedCoords(null)}
+          />
+          <LocateUser
+            onLocationFound={handleLocationFound}
+            userCoords={userCoords}
+          />
+        </MapContainer>
+
+        {/* Barra de búsqueda */}
+        <div className="absolute top-4 left-4 z-10">
+          <SearchBar onSearch={handleSearch} />
+        </div>
+      </div>
+      <SuccessModal
+        name={successMessage || ""}
+        isOpen={!!successMessage}
+        onClose={closeSuccessModal}
       />
     </div>
-
-    {/* Mapa */}
-    <div className="order-1 md:order-2 flex-grow w-full h-2/3 md:h-full">
-      <MapContainer
-        center={[-33.4489, -70.6693]}
-        zoom={9}
-        className="h-full w-full"
-      >
-        <TileLayer
-          attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors &copy; <a href='https://carto.com/'>CARTO</a>"
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
-        />
-        <MarkerList sites={pointData} onDeletePoint={deletePoint} />
-        <CenterMap coords={selectedCoords} />
-        <MapBoundsUpdater
-          points={pointData}
-          setFilteredPoints={setFilteredPoints}
-          resetSelectedCoords={() => setSelectedCoords(null)}
-        />
-        <LocateUser
-          onLocationFound={handleLocationFound}
-          userCoords={userCoords}
-        />
-      </MapContainer>
-
-      {/* Barra de búsqueda */}
-      <div className="absolute top-4 left-4 z-10">
-        <SearchBar onSearch={handleSearch} />
-      </div>
-    </div>
-  </div>
   );
 };
 
