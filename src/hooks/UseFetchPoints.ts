360fff4a9a176a27254792e456187fa5ac4330d4
TomasWielandt <twielandt16@gmail.com>
2025-01-29 21:58:59 -0300

    fix: activepoints updatePoint

--- src/hooks/UseFetchPoints.ts
+++ src/hooks/UseFetchPoints.ts
@@ -122,7 +122,8 @@ const UseFetchPoints = () => {
         // Verifica los datos que estás recibiendo
         console.log("Datos recibidos para actualizar:", data);
             // Eliminar 'id' de los datos antes de enviar
-        const { id, ...formattedData } = data; // Eliminar 'id' de los datos antes de enviar
+        const { id: _, ...formattedData } = data; // Eliminar 'id' de los datos antes de enviar
+        console.log("_:", _);
 
         // Convertir las fechas a ISO 8601 si existen
         formattedData.activationStartDate = formattedData.activationStartDate
